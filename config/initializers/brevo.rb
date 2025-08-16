c70f93cf8fcddf4168d078be7a9b07dcc4a9f10a
furqanameen <furqanbinameen@gmail.com>
2025-08-16 19:30:39 +0100

    Removed testing keys

--- config/initializers/brevo.rb
+++ config/initializers/brevo.rb
@@ -3,11 +3,11 @@ require 'sib-api-v3-sdk'
 
 SibApiV3Sdk.configure do |config|
   # Use the API key from environment variable
-  config.api_key['api-key'] = ENV['BREVO_API_KEY'] || 'xkeysib-1d22370f7cb248ad9bfe6c4a8fc7939965085854b94a2'
+  config.api_key['api-key'] = ENV['BREVO_API_KEY']
 end
 
 Rails.application.config.brevo = {
-  api_key: ENV['BREVO_API_KEY'] || 'xkeysib-1d22370f7cb248ad9bfe6c4a8fc7939965085854b94a2',
+  api_key: ENV['BREVO_API_KEY'],
   # default_sender: {
   #   name: "DevCRM Admin local check brevo config to change it",
   #   email: "info@devhubsol.com"  # Replace with your verified sender email
